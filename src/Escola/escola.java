3b56f27d8d8c0ab01322904964f8e98b3eb865c3
C2002G <claudiogabriel2002@gmail.com>
2025-04-30 15:03:19 -0300

    set/get

--- src/Escola/escola.java
+++ src/Escola/escola.java
@@ -3,9 +3,9 @@ package Escola;
 public class escola {
     public static void main(String[] args) {
         Aluno felipe = new Aluno();
-        felipe.nome = "Felipe";
-        felipe.idade = 8;
+        felipe.setNome("Felipe");
+        felipe.setIdade(8);
 
-        System.out.println("o " + felipe.nome + " tem " + felipe.idade + " anos");
+        System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");
     }
 }
